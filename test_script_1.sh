7ac0c0ade0a04e65ea9f68c3ae1dd7ad4e318b64
Omar <omarjemal@cmail.carleton.ca>
2021-02-05 13:29:32 -0500

    add testing scripts

---
+++ test_script_1.sh
@@ -0,0 +1,12 @@
+
+
+
+
+echo "running test script #1 ...."
+
+./assignment1 input_test_1.txt output_test_1.txt
+
+echo "ran test script #1 ...."
+echo "output file \"output_test_1.txt\" was generated "
+
+
---
+++ test_script_2.sh
@@ -0,0 +1,12 @@
+
+
+
+
+echo "running test script #2 ...."
+
+./assignment1 input_test_2.txt output_test_2.txt
+
+echo "ran test script #2 ..."
+echo "output file \"output_test_2.txt\" was generated "
+
+
