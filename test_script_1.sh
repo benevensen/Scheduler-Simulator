6b8888e1c97b6ffbbaa9af20cf1d7e3428e993e0
Omar <omarjemal@cmail.carleton.ca>
2021-03-05 22:46:37 -0500

    Add test scripts for testing scripts and test input files for testing

--- test_script_1.sh
+++ test_script_1.sh
@@ -1,12 +1,56 @@
 
+# testing script testing all 4 testing input.txt files
+
+# for testing the FCFS scheduler
+
+echo "running test script #1 .... \n"
+
+#----------------------------------------------------------------
+# testing scenario 1
+
+echo "running scenario #1 .... \n"
+
+./assignment2 1 0 input_test_1.txt output_test_1_scenario_1.txt
+
+echo "ran test scenario #1 ...."
+
+echo "output file \"output_test_1_scenario_1.txt\" was generated \n"
 
 
+#----------------------------------------------------------------
+# testing scenario 2
+echo "running scenario #2 .... \n"
 
-echo "running test script #1 ...."
+./assignment2 1 0 input_test_2.txt output_test_1_scenario_2.txt
 
-./assignment1 input_test_1.txt output_test_1.txt
+echo "ran test scenario #2 ...."
 
-echo "ran test script #1 ...."
-echo "output file \"output_test_1.txt\" was generated "
+echo "output file \"output_test_1_scenario_2.txt\" was generated \n"
 
 
+#----------------------------------------------------------------
+# testing scenario 3
+
+echo "running scenario #3 .... \n"
+
+./assignment2 1 0 input_test_3.txt output_test_1_scenario_3.txt
+
+echo "ran test scenario #3 ...."
+
+echo "output file \"output_test_1_scenario_3.txt\" was generated \n"
+
+
+#----------------------------------------------------------------
+# testing scenario 4
+
+echo "running scenario #4 .... \n"
+
+./assignment2 1 0 input_test_4.txt output_test_1_scenario_4.txt
+
+echo "ran test scenario #4 ...."
+
+echo "output file \"output_test_1_scenario_4.txt\" was generated \n"
+
+#----------------------------------------------------------------
+
+echo "finished running test script #1 ...."
---
+++ test_script_3.sh
@@ -0,0 +1,44 @@
+
+# testing script testing first 3 testing input.txt files
+
+# for testing the Round Robin scheduler
+
+echo "running test script #3 .... \n"
+
+#----------------------------------------------------------------
+# testing scenario 8
+
+echo "running scenario #8 .... \n"
+
+./assignment2 3 0 input_test_1.txt output_test_3_scenario_8.txt
+
+echo "ran test scenario #8 ...."
+
+echo "output file \"output_test_3_scenario_8.txt\" was generated \n"
+
+
+#----------------------------------------------------------------
+# testing scenario 9
+echo "running scenario #9 .... \n"
+
+./assignment2 3 0 input_test_2.txt output_test_3_scenario_9.txt
+
+echo "ran test scenario #9 ...."
+
+echo "output file \"output_test_3_scenario_9.txt\" was generated \n"
+
+
+#----------------------------------------------------------------
+# testing scenario 10
+
+echo "running scenario #10 .... \n"
+
+./assignment2 3 0 input_test_3.txt output_test_3_scenario_10.txt
+
+echo "ran test scenario #10 ...."
+
+echo "output file \"output_test_3_scenario_10.txt\" was generated \n"
+
+#----------------------------------------------------------------
+
+echo "finished running test script #3 ...."
